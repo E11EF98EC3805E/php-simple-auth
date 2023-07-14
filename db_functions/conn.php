58d1997d02b06bb73aad4568959e048f702e5d2d
E11EF98EC3805E <137059602+E11EF98EC3805E@users.noreply.github.com>
2023-07-14 13:01:43 +0300

    remove new db

--- db_functions/conn.php
+++ db_functions/conn.php
@@ -1,9 +1,9 @@
 <?php
 	// БД - SQLite 
-	if(!is_file('db/db_user2.sqlite3')){
-		file_put_contents('db/db_user2.sqlite3', null);
+	if(!is_file('db/db_user.sqlite3')){
+		file_put_contents('db/db_user.sqlite3', null);
 	}
-	$conn = new PDO('sqlite:db/db_user2.sqlite3');
+	$conn = new PDO('sqlite:db/db_user.sqlite3');
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$query = "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT UNIQUE, pass TEXT, email TEXT UNIQUE, phonenumber TEXT UNIQUE)";
